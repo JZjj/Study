47d227a79108773ad32fd0bcde9f946d0988e908
Jiajing Zheng <43308829+JZjj@users.noreply.github.com>
2023-10-26 21:24:56 -0400

    Add files via upload

--- C/Exercise/Exercise24.c
+++ C/Exercise/Exercise24.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-    int i =1;
-    double a = 1, total = 0;
-    for(i=1;i<=64;i++){
-        total += a;
-        a = a*2;
-        printf("i=%d, total=%.0lf\n",i,total);
-    }
+    int i, m, flag = 1;
+    printf("Input a postive integer:\n");
+    scanf("%d",&m);
+    for (i = 2; i<= m-1;i++)
+        if(m%i==0){
+            flag = 0;
+        }
+    if (flag ==1 )
+        printf("%d is a prime \n",m);
+    else 
+        printf("%d is not a prime \n",m);
     return 0;
 }
